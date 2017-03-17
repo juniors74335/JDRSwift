e6ba480c83f95bf491d7cd29b0d685dd151d4030
Developer <developer@mac-de-developer-2.local>
2017-03-17 12:18:28 +0100

    Let's roll the dice

--- Application de ouf/ViewController.swift
+++ Application de ouf/ViewController.swift
@@ -19,7 +19,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-//lol
+    
+    @IBOutlet weak var resultDice1: UILabel!
+    @IBOutlet weak var resultDice2: UILabel!
+    @IBOutlet weak var resultDice3: UILabel!
+    @IBOutlet weak var resultDice4: UILabel!
+    @IBOutlet weak var resultDice5: UILabel!
+    
+    
+    @IBOutlet weak var randomDice: UIButton!
+    
+
+    
+    
+    @IBAction func RollDice(_ sender: UIButton) {
+        resultDice1.text = String(Int(arc4random_uniform(UInt32(6)))+7)
+        resultDice2.text = String(Int(arc4random_uniform(UInt32(6)))+7)
+        resultDice3.text = String(Int(arc4random_uniform(UInt32(6)))+7)
+        resultDice4.text = String(Int(arc4random_uniform(UInt32(6)))+7)
+        resultDice5.text = String(Int(arc4random_uniform(UInt32(6)))+7)
+      
+    }	
+    
+
 
 }
 
