84f1076f82f5949012e854480bd4d0f6edae6061
Developer <developer@mac-de-developer-2.local>
2017-03-24 10:47:42 +0100

    Ajout listes Compétences sur classe Origine

--- Application de ouf/Class/Ingenieur.swift
+++ Application de ouf/Class/Ingenieur.swift
@@ -14,6 +14,11 @@ class Ingenieur: Metier {
          
         
         super.init(_statMax: Statistique.init(_force: 14, _charisme: 14, _adresse: 14, _courage: 14, _Intelligence: 14), _statMin: Statistique.init(_force: 7, _charisme: 7, _adresse: 7, _courage: 7, _Intelligence: 7), _metier:"Ingenieur", _listOrigin:list);
+        self.listCompPassive.append("Bricolo du dimanche");
+        self.listCompPassive.append("Désamorcer");
+        self.listCompPassive.append("Forgeron");
+        self.listCompPassive.append("Ressemble à rien");
+        self.listCompPassive.append("Serrurier");
     }
 
 }
--- Application de ouf/Class/Noble.swift
+++ Application de ouf/Class/Noble.swift
@@ -14,6 +14,11 @@ class Noble: Metier {
         
         
         super.init(_statMax: Statistique.init(_force: 14, _charisme: 14, _adresse: 14, _courage: 14, _Intelligence: 14), _statMin: Statistique.init(_force: 7, _charisme: 10, _adresse: 7, _courage: 7, _Intelligence: 9), _metier:"Noble", _listOrigin:list);
+        self.listCompPassive.append("Appel des renforts");
+        self.listCompPassive.append("Attire les monstres");
+        self.listCompPassive.append("Chevaucher");
+        self.listCompPassive.append("Erudition");
+        self.listCompPassive.append("Pénible");
     }
 
 }
